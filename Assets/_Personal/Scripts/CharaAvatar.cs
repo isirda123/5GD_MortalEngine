0dc2bbbc4fa3a55c478bf8ff75d7bf653c988c9c
Isirda123 <isirda123@yahoo.fr>
2020-04-01 17:23:13 +0200

    Moving Camera + Moving Fortress

---
+++ Assets/_Personal/Scripts/CharaAvatar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaAvatar : MonoBehaviour
+{
+    [Tooltip ("Work")]
+    [SerializeField] GameObject workZone;
+    [SerializeField] float rangeWorkZone;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        workZone.transform.localScale = new Vector3( rangeWorkZone, rangeWorkZone, rangeWorkZone);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Personal/Scripts/PlayerInput.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerInput : MonoBehaviour
+{
+    [Tooltip("Click")]
+    float startTime;
+    [SerializeField] float lenghtOfAClick;
+
+    [Tooltip("Camera")]
+    
+    [SerializeField] float speedOfCamera;
+
+    [Tooltip("Avatar")]
+    [SerializeField] GameObject fortressNavMesh;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startTime = Time.time;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time - startTime > lenghtOfAClick)
+            {
+                if (Input.GetAxis("Mouse X") > 0)
+                {
+                    Camera.main.transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speedOfCamera,
+                                               0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speedOfCamera);
+                }
+
+                else if (Input.GetAxis("Mouse X") < 0)
+                {
+                    Camera.main.transform.transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speedOfCamera,
+                                               0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speedOfCamera);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (Time.time - startTime <= lenghtOfAClick)
+            {
+
+
+                //Make the Fortress move;
+                print(Time.time - startTime);
+
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 1000))
+                {
+                    
+                    var clickPosition = hit.point;
+                    print(clickPosition);
+                    fortressNavMesh.GetComponent<NavMeshAgent>().SetDestination(clickPosition);
+                }
+
+                
+            }
+        }
+    }
+}
