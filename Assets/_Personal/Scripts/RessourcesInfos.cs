9bc3ea61df3bf20e647489ec67f720ba0be2aeed
Isirda123 <isirda123@yahoo.fr>
2020-04-02 11:51:42 +0200

    Begin of collectible resources

---
+++ Assets/_Personal/Scripts/RessourcesInfos.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RessourcesInfos : MonoBehaviour
+{
+    public string name;
+    public int resourcesAmount;
+    public int resourcesTimeToMine;
+    public int resourcesTimeToRespawn;
+
+    public void GetDestroy()
+    {
+
+    }
+}
---
+++ Assets/_Personal/Scripts/RessourcesInstanciator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RessourcesInstanciator : MonoBehaviour
+{
+    public List<GameObject> allRessources = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i =0; i < transform.childCount; i++)
+        {
+            allRessources.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator RespawnOfRessources()
+    {
+
+
+        yield return null;
+    }
+
+
+
+}
