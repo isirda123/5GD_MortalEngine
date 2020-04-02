52a2660094a4b96b86e4d46fea8365bd73e4da8d
Dangonok <dangonok@gmail.com>
2020-04-02 11:15:40 +0200

    Game Manager & resources types

---
+++ Assets/_Personal/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum ResourceType
+    {
+        Wood,
+        Sheep,
+        Corn,
+        Rock
+    }
+}
