d774f75cde8936da93978c77643aa703ca3f8f1d
Dangonok <dangonok@gmail.com>
2020-04-23 10:58:35 +0200

    change name to type

--- Assets/_Personal/Scripts/RessourcesInfos.cs
+++ Assets/_Personal/Scripts/RessourcesInfos.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RessourcesInfos : MonoBehaviour
 {
     //[System.Serializable]
-    public new GameManager.ResourceType name;
+    public new GameManager.ResourceType type;
     public float resourcesTimeToMine;
     public float resourcesTimeToRespawn;
     public float resourcesAmount;
