73c08f69839e586e14029bbba4233728f5412cb6
Dangonok <dangonok@gmail.com>
2020-06-02 08:39:25 +0200

    level editor shortcup ctrl+g

---
+++ Assets/_Personal/Scripts/LevelEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class LevelEditor : MonoBehaviour
+{
+    [MenuItem("MyMenu/Do Something with a Shortcut Key %g")]
+    static void DoSomethingWithAShortcutKey()
+    {
+        if (Selection.transforms[0].CompareTag("Hexagone"))
+        {
+            Selection.transforms[0].parent.GetComponent<TilesManager>().SetAllTiles();
+        }
+        Debug.Log("Set tiles");
+    }
+}
